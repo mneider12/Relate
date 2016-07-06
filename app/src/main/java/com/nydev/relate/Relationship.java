0d8b413f4854510a8595c2e41ea9ca180ec0f774
markneider <mneider.virginia@gmail.com>
2016-07-06 14:07:41 -0500

    Add some getter and setter methods to Relationship

--- app/src/main/java/com/nydev/relate/Relationship.java
+++ app/src/main/java/com/nydev/relate/Relationship.java
@@ -1,8 +1,7 @@
 package com.nydev.relate;
 
-import android.support.annotation.NonNull;
-
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * Created by markneider on 7/2/16.
@@ -11,11 +10,11 @@ public class Relationship implements Serializable
 {
     int id;
     private String name;
-    private String date;
+    private Date birthday;
     private String relationship;
     private String note;
 
-    public Relationship(int id, String name, String date, String relationshipDescription, String note)
+    public Relationship(int id, String name, Date birthday, String relationshipDescription, String note)
     {
         this.id = id;
         if (name == null)
@@ -26,7 +25,7 @@ public class Relationship implements Serializable
         {
             this.name = name;
         }
-        this.date = date;
+        this.birthday = birthday;
         this.relationship = relationshipDescription;
         this.note = note;
     }
@@ -51,5 +50,30 @@ public class Relationship implements Serializable
     public String getName()
     {
         return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public Date getBirthday()
+    {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday)
+    {
+        this.birthday = birthday;
+    }
+
+    public String getRelationship()
+    {
+        return relationship;
+    }
+
+    public String getNote()
+    {
+        return note;
     }
 }
