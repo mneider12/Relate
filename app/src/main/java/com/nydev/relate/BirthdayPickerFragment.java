f5318d8a44a1441f6c5bf5650f217437dd9d781e
markneider <mneider.virginia@gmail.com>
2016-08-02 21:00:16 -0500

    Replace birthday calender picker with custom Dialog

--- app/src/main/java/com/nydev/relate/BirthdayPickerFragment.java
+++ app/src/main/java/com/nydev/relate/BirthdayPickerFragment.java
@@ -5,11 +5,54 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.DatePicker;
+
+import org.joda.time.MonthDay;
 
 import java.util.Calendar;
 
-public class BirthdayPickerFragment extends DialogFragment
+public class BirthdayPickerFragment extends DialogFragment {
+
+
+
+    public static BirthdayPickerFragment newInstance(MonthDay initialDate) {
+        final String MONTH_KEY = "month";
+        final String DAY_KEY = "day";
+
+        BirthdayPickerFragment birthdayPickerFragment = new BirthdayPickerFragment();
+
+        if (initialDate == null) {
+            initialDate = new MonthDay();
+        }
+        Bundle birthdayArgs = new Bundle();
+        birthdayArgs.putInt(MONTH_KEY, initialDate.getMonthOfYear());
+        birthdayArgs.putInt(DAY_KEY, initialDate.getDayOfMonth());
+        birthdayPickerFragment.setArguments(birthdayArgs);
+
+        return birthdayPickerFragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstance) {
+        View birthdayPickerDialogView = inflater.inflate(R.layout.birthday_picker_dialog, container,
+                false);
+
+        return birthdayPickerDialogView;
+    }
+}
+
+/*public class BirthdayPickerFragment extends DialogFragment
         implements DatePickerDialog.OnDateSetListener {
 
     DatePickerDialog.OnDateSetListener mOnDatePickedListener;
@@ -42,3 +85,4 @@ public class BirthdayPickerFragment extends DialogFragment
         mOnDatePickedListener.onDateSet(datePicker, year, month, day);
     }
 }
+*/
